f32fdd413a626990925e1e7537d38653348e4cc6
Alex Pickering <alexvpickering@gmail.com>
2024-10-03 17:49:35 -0700

    add example using GRanges data

---
+++ data-raw/examples/gr_ranges_data.R
@@ -0,0 +1,47 @@
+url <- "https://rb.gy/7y3fx"
+temp_file <- file.path(tempdir(), "data.gz")
+download.file(url, destfile = temp_file, method = "auto", mode = "wb")
+df <- read.delim(
+  temp_file,
+  header = FALSE,
+  comment.char = "#",
+  sep = ""
+)
+
+gr <- GRanges(
+  seqnames = df$V1,
+  ranges = IRanges(df$V2, df$V3, peak = df$V5)
+)
+
+gr
+
+# Extract data from GRanges object
+values <- as.data.frame(gr)
+
+# Create the desired JSON structure as a list
+gr_data <- list(
+  type = "json",
+  chromosomeField = "seqnames",
+  genomicFields = c("start", "end"),
+  values = values
+)
+
+spec <- list(
+  title = "Basic Marks: bar",
+  subtitle = "Tutorial Examples",
+  tracks = list(
+    list(
+      layout = "linear",
+      width = 800,
+      height = 180,
+      data = gr_data,
+      mark = "bar",
+      x = list(field = "start", type = "genomic", axis = "bottom"),
+      xe = list(field = "end", type = "genomic"),
+      y = list(field = "peak", type = "quantitative", axis = "right"),
+      size = list(value = 5)
+    )
+  )
+)
+
+goslingr(spec)
