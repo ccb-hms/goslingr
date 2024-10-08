d00a0e405a1bf71bf87dd1ef456217e60c334800
Alex Pickering <alexvpickering@gmail.com>
2024-10-08 10:02:01 -0700

    adjust example

--- data-raw/examples/gr_ranges_data.R
+++ data-raw/examples/gr_ranges_data.R
@@ -1,3 +1,6 @@
+library(GenomicRanges)
+library(goslingr)
+
 url <- "https://rb.gy/7y3fx"
 temp_file <- file.path(tempdir(), "data.gz")
 download.file(url, destfile = temp_file, method = "auto", mode = "wb")
@@ -44,4 +47,25 @@ spec <- list(
   )
 )
 
+print_list(spec)
 goslingr(spec)
+
+
+# simpler example GRanges object
+
+library(GenomicRanges)
+
+gr <- GRanges(
+  seqnames = paste0('chr', c(1:22, 'X', 'Y')),
+  peak = rnorm(24, mean = 500, sd = 100),
+  ranges = IRanges(
+    start = rep(0, 24),
+    end = rep(10, 24)
+  )
+)
+
+# create a bar chart spec
+spec <- bar_chart_spec(gr)
+
+# print it for copying/editing in R
+print(spec)
